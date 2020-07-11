35dcb3351726cf0849e4236ec2e20b73e2311a32
Michael Drogalis <michael.drogalis@confluent.io>
2020-07-11 16:51:46 -0700

    Save

--- dynamic.js
+++ dynamic.js
@@ -28,7 +28,24 @@ function render_persistent_query(styles) {
   $(".system").append(html);
 }
 
-function render_input(inputs, styles, computed) {
+function render_rows(rows, styles, computed) {
+  ({ row_width, row_height, row_margin_left, row_offset_right } = styles);
+  ({ part_height } = styles);
+  ({ right_x, top_y } = computed);
+
+  let row_x = right_x - row_offset_right - row_width;
+  const row_y = top_y + (part_height / 2) - (row_height / 2);
+
+  let row_html = "";
+  for (const row of rows) {
+    row_html += `<rect height="${row_height}" width="${row_width}" x="${row_x}" y="${row_y}" class="row"></rect>`;
+    row_x -= (row_width + row_margin_left);
+  }
+
+  return row_html;
+}
+
+function render_partition(rows, styles, computed) {
   ({ part_id_margin_top, part_id_margin_left } = styles);
   ({ part, top_y } = computed);
 
@@ -39,23 +56,27 @@ function render_input(inputs, styles, computed) {
   const right_x = midpoint_x + (styles.part_width / 2);
   const bottom_y = top_y + styles.part_height;
 
-  const html = `<g class="partition-container">
-        <text x="${left_x + part_id_margin_left}" y="${top_y + part_id_margin_top}" class="code">${part}</text>
+  const part_html = `
+        <text x="${left_x + part_id_margin_left}" y="${top_y + part_id_margin_top}" class="code">${part}</text>`;
 
+  const bracket_html = `
         <path d="M ${left_x + b_len},${top_y} h ${-b_len} v ${b_len}" class="partition"></path>
         <path d="M ${right_x - b_len},${top_y} h ${b_len} v ${b_len}" class="partition"></path>
         <path d="M ${left_x},${bottom_y - b_len} v ${b_len} h ${b_len}" class="partition"></path>
-        <path d="M ${right_x},${bottom_y - b_len} v ${b_len} h ${-b_len}" class="partition"></path>
-     </g>`;
+        <path d="M ${right_x},${bottom_y - b_len} v ${b_len} h ${-b_len}" class="partition"></path>`;
+
+  const rows_html = render_rows(rows, styles, {right_x: right_x, top_y: top_y});
+
+  const html = `<g class="partition-container">${part_html}${bracket_html}${rows_html}</g>`;
   $(".system").append(html);
 }
 
-function render_inputs(inputs, styles) {
+function render_partitions(inputs, styles) {
   let top_y = 0;
   ({ part_height, part_margin_bottom } = styles);
   
-  for (const [key, value] of Object.entries(inputs.s1)) {
-    render_input(inputs, styles, {part: key, top_y: top_y});
+  for (const [partition, rows] of Object.entries(inputs.s1)) {
+    render_partition(rows, styles, {part: partition, top_y: top_y});
     top_y += (part_height + part_margin_bottom);
   }
 }
@@ -75,7 +96,12 @@ const context = {
     part_margin_bottom: 25,
     part_bracket_len: 10,
     part_id_margin_left: -15,
-    part_id_margin_top: 8
+    part_id_margin_top: 8,
+
+    row_width: 15,
+    row_height: 15,
+    row_margin_left: 10,
+    row_offset_right: 15
   },
   inputs: {
     s1: {
@@ -109,7 +135,7 @@ const context = {
 $(document).ready(function() {
   render_svg(context.styles);
   render_persistent_query(context.styles);
-  render_inputs(context.inputs, context.styles);
+  render_partitions(context.inputs, context.styles);
 
   // Repaint.
   $(".system").html($(".system").html());
